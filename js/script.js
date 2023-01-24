c02ce2c6beaceb83db1616af2fe8a87dcb4d0522
sean <srosen6136@mpc.edu>
2023-01-23 21:50:18 -0800

    improved enemy movement

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
 var enemyNode = document.getElementById('enemy-node');
-var enemy = document.createElement("div");
+var enemyNodeTwo = document.getElementById('enemy-node2');
+var enemyNodethree = document.getElementById('enemy-node3');
+var enemyNodefour = document.getElementById('enemy-node4');
+var enemyNodefive = document.getElementById('enemy-node5');
+var enemyNodes = [enemyNode, enemyNodeTwo, enemyNodethree, enemyNodefour, enemyNodefive];
+var enemies = [];
 enemyNode.addEventListener("click", function () {
-    enemy.classList.add("enemy");
-    enemyNode.appendChild(enemy);
+    var css = window.document.styleSheets[0];
+    for (var i = 1; i <= 6; i++) {
+        var enemy = document.createElement("div");
+        enemies[i] = enemy;
+        enemies[i].classList.add("enemy");
+        enemyNodes[i].appendChild(enemies[i]);
+        enemies[i].style.animation = "myAnimation " + (i * 3) + "s ease infinite";
+        css.insertRule("\n@keyframes myAnimation {\n  0%   { top: 0;     }\n  100% { top: 2000%; }\n}", css.cssRules.length);
+    }
 });
-enemy.addEventListener("animationend", function () {
-    enemy.parentNode.removeChild(enemy);
-});
-enemy.addEventListener("click", function () {
-    enemy.parentNode.removeChild(enemy);
-});
+// enemies[].addEventListener("animationend", () => {
+//     enemies[i].parentNode.removeChild(enemies[i]);
+// });
+// enemy.addEventListener("click", () => {
+//     enemy.parentNode.removeChild(enemy);
+// });
--- js/script.ts
+++ js/script.ts
@@ -1,19 +1,31 @@
 let enemyNode = document.getElementById('enemy-node');
-let enemy = document.createElement("div");
+let enemyNodeTwo = document.getElementById('enemy-node2');
+let enemyNodethree = document.getElementById('enemy-node3');
+let enemyNodefour = document.getElementById('enemy-node4');
+let enemyNodefive = document.getElementById('enemy-node5');
+let enemyNodes = [enemyNode, enemyNodeTwo, enemyNodethree,enemyNodefour, enemyNodefive];
+let enemies = [];
+
 
 
 enemyNode.addEventListener("click", () => {
-    enemy.classList.add("enemy");
-    enemyNode.appendChild(enemy);
+    const css = window.document.styleSheets[0];
+    for (let i = 0; i <= 5; i++) {
+        let enemy = document.createElement("div");
+        enemies[i] = enemy;
+        enemies[i].classList.add("enemy");
+        enemyNodes[i].appendChild(enemies[i]);
+        enemies[i].style.animation = "myAnimation " + (5 + i) + "s ease infinite";
+       
+        css.insertRule(`
+@keyframes myAnimation {
+  0%   { top: 0;     }
+  100% { top: 2000%; }
+}`, css.cssRules.length);
+    }
 });
 
 
-enemy.addEventListener("animationend", () => {
-    enemy.parentNode.removeChild(enemy);
-});
-enemy.addEventListener("click", () => {
-    enemy.parentNode.removeChild(enemy);
-});
 
 
 
